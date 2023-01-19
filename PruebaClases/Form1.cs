bf97196e7d713ffe833d21b89958d5fb48840480
Jorgevillegash7 <a00826379@tec.mx>
2023-01-19 08:13:58 -0600

    Add project files.

---
+++ PruebaClases/Form1.cs
@@ -0,0 +1,121 @@
+﻿using PruebaClases.MetodosAlgoritmos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using objExcel = Microsoft.Office.Interop.Excel;
+
+namespace PruebaClases
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Paso 1: Condicíon de vacío
+                if (textBox1.Text.Equals("") ||
+                    textBox2.Text.Equals("") || textBox3.Text.Equals(""))
+
+                {
+                    MessageBox.Show("Los números tienen que ser MAYOR que cero, NO VACÍOS");
+                    return;
+                }
+                int numeroDatos = Convert.ToInt32(textBox1.Text);
+                int limiteInferior = Convert.ToInt32(textBox2.Text);
+                int limiteSuperior = Convert.ToInt32(textBox3.Text);
+                List<double> numeros = new List<double>();  
+                double media = 0;
+                double varianza = 0;
+                double desviacionEstandar = 0;
+                // Paso 2: Condicíon de Mayor y Menor
+                if (limiteInferior >= limiteSuperior)
+                {
+                    MessageBox.Show("Los límites tienen error");
+                    return;
+                }
+                if (limiteInferior > 0 && limiteSuperior > 0 && numeroDatos > 0)
+                {
+                    Tarea2 algoritmo = new Tarea2();
+                    media = algoritmo.AlgoritmoGenerarAleatoriosVarianza(numeroDatos, limiteInferior, limiteSuperior,numeros);
+
+
+                    llenarGrid(numeroDatos, algoritmo);
+                    textBox4.Text = numeros[0].ToString();
+                    textBox5.Text = numeros[1].ToString();
+                    textBox6.Text = numeros[2].ToString();
+                    for (int i = 0; i < numeroDatos; i++);
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Vuelve a intentar (Núnca te rindas)");
+                    return;
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Vuelve a intentar");
+            }  
+        }
+     
+    
+
+        public void llenarGrid(int numeroDatos, Tarea2 algoritmo) 
+        {
+            string numeroColumna1 = "1";
+            string numeroColumna2 = "2";
+
+            dataGridView1.Columns.Clear();
+            dataGridView1.Columns.Add(numeroColumna1, "Algoritmo 1");
+            dataGridView1.Columns.Add(numeroColumna2, "ID");
+            for (int i=0; i< numeroDatos;i ++) 
+            {
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[i].Cells[Int32.Parse(numeroColumna1)-1].Value = algoritmo.listaDemandas[i].CantidadRequerida.ToString();
+                dataGridView1.Rows[i].Cells[Int32.Parse(numeroColumna2)-1].Value = algoritmo.listaDemandas[i].IdDemanda.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+             DescargaExcel( dataGridView1);
+        }
+        public void DescargaExcel(DataGridView data) 
+        {
+            Microsoft.Office.Interop.Excel.Application exportarExcel = new Microsoft.Office.Interop.Excel.Application();
+            exportarExcel.Application.Workbooks.Add(true);
+            int indiceColumna = 0;
+            // Llenar (construir) encabezados
+            foreach(DataGridViewColumn columna in data.Columns) 
+            {
+                indiceColumna= indiceColumna+1;
+                exportarExcel.Cells[1, indiceColumna] = columna.HeaderText;
+            }
+            // Llenar filas
+            int indiceFila = 0;
+            foreach (DataGridViewRow fila in data.Rows) 
+            {
+                indiceFila++;
+                indiceColumna =0;
+                foreach (DataGridViewColumn columna in data.Columns)
+                {
+                    indiceColumna++;
+                    exportarExcel.Cells[indiceFila+1, indiceColumna] = fila.Cells[columna.Name].Value;
+                }
+            }
+            exportarExcel.Visible= true;
+        }
+    }
+}
---
+++ PruebaClases/MetodosAlgoritmos/Tarea2.cs
@@ -0,0 +1,120 @@
+﻿using PruebaClases.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaClases.MetodosAlgoritmos
+{
+    public class Tarea2
+    {
+        public List<Demanda> listaDemandas = new List<Demanda>();
+        public Tarea2()
+        {
+        }
+        // Objetivo: Método realizará el promedio de una lista de demandas
+        public double CalcularMedia(List<Demanda> listaDem)
+        {
+            // Paso 1: Declarar variables y setearlas
+            double media = 0, suma = 0;
+            // Paso 2: Calcular la variable suma (ACUMULADO)
+            foreach (Demanda demanda2 in listaDem)
+            {
+                suma = suma + demanda2.suma;
+            }
+            // Paso 3: Dividir entre total de datos (PROMEDIO)
+            // Paso 3.1: Condicionar a tener datos en la listaDem
+            if (listaDem.Count() > 0)
+            {
+                media = suma / listaDem.Count();
+            }
+            return media;
+        }
+        public double CalcularVarianza(List<Demanda> listaDem, double media)
+        {
+            // Paso 1: Declarar variables y setearlas
+            double varianza = 0, suma = 0;
+            // Paso 2: Calcular la variable suma (ACUMULADO)
+            foreach (Demanda demanda2 in listaDem)
+            {
+                suma =  suma + (demanda2.suma - media) * (demanda2.suma - media);
+            }
+            // Paso 3: Dividir entre total de datos (PROMEDIO)
+            // Paso 3.1: Condicionar a tener datos en la listaDem
+            if (listaDem.Count() > 0)
+            {
+                varianza = suma / listaDem.Count();
+            }
+            return varianza;
+        }
+        public double CalcularDesviacionEstandar(double varianza)
+        {
+            // Paso 1: Declarar variables y setearlas
+            double desviacionEstandar = 0;
+            // Paso 2: Calcular la variable suma (ACUMULADO)
+            
+                desviacionEstandar = Math.Sqrt(varianza);
+            
+            // Paso 3: Dividir entre total de datos (PROMEDIO)
+          
+             return desviacionEstandar;
+        }
+        // Llena datos de manera aleatorio Uniforme
+        public void LlenarDatosAleatorios(int numeroDatos,
+             int limiteInferior, int limiteSuperior)
+        {
+            // Paso 1: Declarar la variable aleatorio (Entrada un tiempo)
+            Random aleatorio = new Random(Environment.TickCount);
+            // Paso 2: Realizar un ciclo, para generar una cantidad "numeroDatos" de valores aleatorios
+            for (int i = 0; i < numeroDatos; i++)
+            {
+                // (Generar valores aleatorios entre cero y uno)
+                double value = aleatorio.NextDouble();
+                // Paso 2.1:  Generar valor aleatorio (VARIABLE) entre limite inferior y limite superior (Entero)
+                double value2 = aleatorio.Next(limiteInferior, limiteSuperior);
+                double value3 = aleatorio.Next(limiteInferior, limiteSuperior);
+
+                // Paso 2.2:  Declarar e inicializar variable "demanda"
+                Demanda demanda = new Demanda();
+                // Paso 2.3: Settear el atributo "idDemanda" con la variable "i" (castear)
+                demanda.IdDemanda = i.ToString();
+                // Paso 2.4: Asignar el "value2" a cantidad requerida 
+                demanda.CantidadRequerida = value2;
+                demanda.suma = value2 + value3;
+
+                // Paso 2.5: Agregar el objeto demanda a la listaDemanda 
+                listaDemandas.Add(demanda);
+            }
+        }
+
+        public double AlgoritmoGenerarAleatoriosMedia(int numeroDatos,
+            int limiteInferior, int limiteSuperior)
+        {
+
+            double media = 0;
+
+            LlenarDatosAleatorios(numeroDatos, limiteInferior, limiteSuperior);
+
+            media = CalcularMedia(listaDemandas);
+
+            return media;
+        }
+        public double AlgoritmoGenerarAleatoriosVarianza(int numeroDatos,
+           int limiteInferior, int limiteSuperior,List<double>listadatos)
+        {
+            double Varianza = 0;
+            double media = 0;
+            double desviacionEstandar = 0;
+
+            LlenarDatosAleatorios(numeroDatos, limiteInferior, limiteSuperior);
+            media = CalcularMedia(listaDemandas);
+            Varianza = CalcularVarianza(listaDemandas,media);
+            desviacionEstandar = CalcularDesviacionEstandar(Varianza);
+            listadatos.Add(media);
+            listadatos.Add(Varianza);
+            listadatos.Add(desviacionEstandar);
+            return Varianza;
+        }
+    }
+}
